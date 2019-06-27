aae02bece5e743c817bfc88fed1a31f9bc32173f
Caleb Severn <calsev@gmail.com>
2019-06-27 15:36:52 -0800

    Added library files

---
+++ libraries/sqlite_install_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# This module implements helpers that are used for resources
+module SqliteInstall
+  # This module implements helpers that are used for resources
+  module Helper
+  end
+end

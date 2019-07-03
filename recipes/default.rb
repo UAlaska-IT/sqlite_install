df11b5da1c130e77b00cab954eb3340b08534ca2
Caleb Severn <calsev@gmail.com>
2019-07-03 10:37:29 -0800

    Uplifted install of tcl

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+dev =
+  if node['platform_family'] == 'debian'
+    'dev'
+  else
+    'devel'
+  end
+
 apt_update 'Pre-Install Update' do
   action :update
 end
@@ -8,3 +15,5 @@ package 'gcc'
 package 'g++' if node['platform_family'] == 'debian'
 package 'gcc-c++' unless node['platform_family'] == 'debian'
 package 'make'
+
+package "tcl-#{dev}"

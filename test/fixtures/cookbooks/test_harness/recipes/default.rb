79d0f8ed89923dba4d44ea75a82e4b02da8aa570
Caleb Severn <calsev@gmail.com>
2019-07-03 09:23:23 -0800

    Fixed relative paths in recipe

--- test/fixtures/cookbooks/test_harness/recipes/default.rb
+++ test/fixtures/cookbooks/test_harness/recipes/default.rb
@@ -4,19 +4,19 @@ include_recipe 'sqlite_install::default'
 
 sqlite_installation 'All Defaults'
 
-directory 'usr/local/sqlite-dl'
+directory '/usr/local/sqlite-dl'
 
-directory 'usr/local/sqlite-bld'
+directory '/usr/local/sqlite-bld'
 
-directory 'usr/local/sqlite'
+directory '/usr/local/sqlite'
 
 user 'bud'
 
 sqlite_installation 'No Defaults' do
   year '2018'
   version '3.26.0'
-  download_directory 'usr/local/sqlite-dl'
-  build_directory 'usr/local/sqlite-bld'
+  download_directory '/usr/local/sqlite-dl'
+  build_directory '/usr/local/sqlite-bld'
   install_directory '/usr/local/sqlite'
   owner 'bud'
   group 'bud'

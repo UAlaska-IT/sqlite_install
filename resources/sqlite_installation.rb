f9d9b67b4e1042a787a013fd299465c930ff95ac
Caleb Severn <calsev@gmail.com>
2019-07-02 17:39:09 -0800

    Changed default install directory to nil

--- resources/sqlite_installation.rb
+++ resources/sqlite_installation.rb
@@ -9,7 +9,7 @@ property :year, String, default: '2019'
 property :version, String, default: '3280000'
 property :download_directory, [String, nil], default: nil
 property :build_directory, [String, nil], default: nil
-property :install_directory, String, name_property: true
+property :install_directory, [String, nil], default: nil
 property :owner, String, default: 'root'
 property :group, String, default: 'root'
 

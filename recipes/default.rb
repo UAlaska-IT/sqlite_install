2b8ae4dad493982b0f58d6fc6d79fce9bb1a96ba
Caleb Severn <calsev@gmail.com>
2019-07-03 09:33:25 -0800

    Added apt update

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+apt_update 'Pre-Install Update' do
+  action :update
+end
+
 package 'gcc'
 package 'g++' if node['platform_family'] == 'debian'
 package 'gcc-c++' unless node['platform_family'] == 'debian'

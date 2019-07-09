a33fc2215f56940e309333ed49e6015dbed91173
Caleb Severn <calsev@gmail.com>
2019-07-08 17:37:21 -0800

    Added tests for directories and user

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -32,3 +32,35 @@ end
 describe package("tcl-#{dev}") do
   it { should be_installed }
 end
+
+describe file('/usr/local/sqlite-dl') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/usr/local/sqlite-bld') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/usr/local/sqlite') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe user('bud') do
+  it { should exist }
+  its('group') { should eq 'bud' }
+  its('groups') { should eq ['bud'] }
+  its('home') { should eq '/home/bud' }
+  its('shell') { should eq '/bin/sh' }
+end

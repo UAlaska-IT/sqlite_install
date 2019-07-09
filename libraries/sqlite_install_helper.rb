bbf99af237dd534108d859529c93fe2d36d02bb9
Caleb Severn <calsev@gmail.com>
2019-07-09 09:22:36 -0800

    Now setting owner of archive

--- libraries/sqlite_install_helper.rb
+++ libraries/sqlite_install_helper.rb
@@ -56,11 +56,13 @@ module SqliteInstall
       return file
     end
 
-    def download_archive(version, new_resource)
+    def download_archive(version, owner, group, new_resource)
       download_file = path_to_download_file(new_resource.download_directory, version)
       url = download_url(version, new_resource)
       remote_file download_file do
         source url
+        owner owner
+        group group
       end
       return download_file
     end
@@ -122,7 +124,7 @@ module SqliteInstall
     end
 
     def extract_archive(new_resource, build_directory, user, group, version)
-      download_file = download_archive(version, new_resource)
+      download_file = download_archive(version, user, group, new_resource)
       manage_source_directory(download_file, version, build_directory, user, group)
       extract_download(download_file, build_directory, user, group)
     end

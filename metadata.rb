516f660bacf9547b32bea247210f6db98ef53ca5
Caleb Severn <calsev@gmail.com>
2019-10-05 13:54:33 -0800

    Drop support for ubuntu 14

--- metadata.rb
+++ metadata.rb
@@ -13,7 +13,7 @@ issues_url "#{git_url}/issues" if respond_to?(:issues_url)
 
 version '1.1.1'
 
-supports 'ubuntu', '>= 14.0'
+supports 'ubuntu', '>= 16.0'
 supports 'debian', '>= 8.0'
 supports 'redhat', '>= 6.0'
 supports 'centos', '>= 6.0'
